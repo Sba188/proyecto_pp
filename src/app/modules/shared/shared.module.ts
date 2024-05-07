aa15a191f86671202919c2a1f091b445da3ec3f4
ismael2407 <ismaellescano550@gmail.com>
2024-05-07 10:59:19 -0300

    agregue el navbar con algunos iconos

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+
+//COMPONENTES LOCALES Y GLOBALES
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+
+
+
+//COMPONENTES DE ANGULAR
+
+//NAVBAR
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+
 
 
 
@@ -11,7 +25,18 @@ import { FooterComponent } from './components/footer/footer.component';
     FooterComponent
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    AppRoutingModule
+  ],
+  exports:[
+    NavbarComponent,
+    FooterComponent,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
 export class SharedModule { }
